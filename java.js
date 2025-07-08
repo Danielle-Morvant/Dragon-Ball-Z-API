12389d3557484c8d64f2c7ca27e2107e61438799
Danielle-Morvant <daniellemorvant1@gmail.com>
2025-07-08 00:14:33 -0500

    console log functioning correctly

--- java.js
+++ java.js
@@ -5,14 +5,20 @@ fecthData();
 async function fecthData(){
 
     try{
+
+        const dragonName = document.getElementById("dragonName").value;
+
+
         const response = await fetch("https://dragonball-api.com/api/characters?limit=58");
 
         if(!response.ok){
             throw new Error("can't do that!");
         }
 
-        const data = await response.json();
-        console.log(data);
+       const data = await response.json();
+       console.log(data);
+       return data.items;
+   
     }
     catch(error){
         console.log(error);
