0cd79f5aeb54db0e716be597907e737fdf5910d0
Danielle-Morvant <daniellemorvant1@gmail.com>
2025-07-07 21:01:37 -0500

    try catch complete and console logged

--- java.js
+++ java.js
@@ -1,5 +1,20 @@
 console.log("I Am The Answer To All Living Things That Cry Out For Peace. -Goku!")
 
-fetch("https://dragonball-api.com/api/characters")
-   .then(response => console.log(response))
-   .catch(error => console.error(error));
+fecthData();
+
+async function fecthData(){
+
+    try{
+        const response = await fetch("https://dragonball-api.com/api/characters?limit=58");
+
+        if(!response.ok){
+            throw new Error("can't do that!");
+        }
+
+        const data = await response.json();
+        console.log(data);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
