99df031e2edd77837af7dbb4bb29bfb282c4404d
Danielle-Morvant <daniellemorvant1@gmail.com>
2025-07-07 20:09:12 -0500

    tested api url and it recieved a 200

--- java.js
+++ java.js
@@ -1,1 +1,5 @@
 console.log("I Am The Answer To All Living Things That Cry Out For Peace. -Goku!")
+
+fetch("https://dragonball-api.com/api/characters")
+   .then(response => console.log(response))
+   .catch(error => console.error(error));
