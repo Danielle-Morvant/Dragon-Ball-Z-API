177166b3aff2f3b3f6f1654eece93139cbf975eb
Danielle-Morvant <daniellemorvant1@gmail.com>
2025-07-09 17:26:30 -0500

    ADD DETAIL FUNCTION COMMENTS IN js starting 2ND FUNCTION

--- java.js
+++ java.js
@@ -2,8 +2,12 @@ console.log("I Am The Answer To All Living Things That Cry Out For Peace. -Goku!
 
 
       //VARIABLES DECLARED HERE
+
+        //BUTTON ID = bttn
         const bttn = document.getElementById("bttn");
+        //IMG CONTAINER ID = dbzCharacter
         const dbzCharacter = document.getElementById("dbzCharacter");
+        //TEXT INPUT ID =  dragonName
         const dragonName = document.getElementById("dragonName");
 
 
@@ -16,13 +20,13 @@ async function fecthData(){
        
        //CONNECTS TO THE WEBSITE AND STORES THE RESULTS IN RESPONSE
         const response = await fetch("https://dragonball-api.com/api/characters?limit=58");
-       
+      
 
         if(!response.ok){
             throw new Error("can't do that!");
         }
 
-      //CONVERTS THE RESULTS FROM STRING FORM TO OBJECT OR DATA FORM
+       //CONVERTS THE RESULTS FROM STRING FORM TO OBJECT OR DATA 
        const data = await response.json();
        console.log(data.items);
    
@@ -33,6 +37,26 @@ async function fecthData(){
     }
 
     
-}
+};
 
-document.getElementById("bttn").addEventListener("click",fecthData);
+
+//function that randomnly picks one character with image when name is typed and button is clicked
+async function randomCharacter(){
+
+    try 
+
+    const response = await fetcch("https://dragonball-api.com/api");
+
+     if(!response.ok){
+            throw new Error("can't do that!");
+        }
+
+
+    
+
+    
+
+    
+}
+       //ACTIVATE FUNCTIONS
+       document.getElementById("bttn").addEventListener("click",fecthData);
