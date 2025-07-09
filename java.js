264443cf712e3b8fb9ac5d3882e6bc2407fce7dd
Danielle-Morvant <daniellemorvant1@gmail.com>
2025-07-09 12:15:32 -0500

    add variable for button and other elements

--- java.js
+++ java.js
@@ -6,21 +6,26 @@ async function fecthData(){
 
     try{
 
-        const dragonName = document.getElementById("dragonName").value;
-
-
+        const bttn = document.getElementById("bttn");
+        const dbzCharacter = document.getElementById("dbzCharacter");
+        const dragonName = document.getElementById("dragonName");
         const response = await fetch("https://dragonball-api.com/api/characters?limit=58");
 
         if(!response.ok){
             throw new Error("can't do that!");
         }
 
+
        const data = await response.json();
-       console.log(data);
-       return data.items;
+       console.log(data.items);
    
     }
     catch(error){
         console.log(error);
     }
+
+    bttn.addEventListener.fecthData()
+    
+
 }
+
