943a410eef2430a956b5902f584ec5a1a53a53a4
Danielle-Morvant <daniellemorvant1@gmail.com>
2025-07-09 21:41:33 -0500

    wrote more functions and tested in console got rid of addEventListener in html

--- java.js
+++ java.js
@@ -1,66 +1,70 @@
-console.log("I Am The Answer To All Living Things That Cry Out For Peace. -Goku!")
+console.log(
+  "I Am The Answer To All Living Things That Cry Out For Peace. -Goku!"
+);
 
+//VARIABLES DECLARED HERE
 
-      //VARIABLES DECLARED HERE
+//BUTTON ID = bttn
+const bttn = document.getElementById("bttn");
+//IMG CONTAINER ID = dbzCharacter
+const dbzCharacter = document.getElementById("dbzCharacter");
+//TEXT INPUT ID =  dragonName
+const dragonName = document.getElementById("dragonName");
 
-        //BUTTON ID = bttn
-        const bttn = document.getElementById("bttn");
-        //IMG CONTAINER ID = dbzCharacter
-        const dbzCharacter = document.getElementById("dbzCharacter");
-        //TEXT INPUT ID =  dragonName
-        const dragonName = document.getElementById("dragonName");
+bttn.addEventListener("click", () => showCharacters());
+fetchData("Goku");
 
+async function fetchData(name) {
+  try {
+    //CONNECTS TO THE WEBSITE AND STORES THE RESULTS IN RESPONSE
+    const response = await fetch(
+      `https://dragonball-api.com/api/characters?name=${name}`
+    );
 
-fecthData();
-
-async function fecthData(){
-
-    try{
-       
-       
-       //CONNECTS TO THE WEBSITE AND STORES THE RESULTS IN RESPONSE
-        const response = await fetch("https://dragonball-api.com/api/characters?limit=58");
-      
-
-        if(!response.ok){
-            throw new Error("can't do that!");
-        }
-
-       //CONVERTS THE RESULTS FROM STRING FORM TO OBJECT OR DATA 
-       const data = await response.json();
-       console.log(data.items);
-   
-    }
-    catch(error){
-        //IF THERES AN ERROR, RUN THIS CODE
-        console.log("SOMETHING IS BUGGIN` DA CODE:",error);
+    if (!response.ok) {
+      throw new Error("can't do that!");
     }
 
-    
-};
-
+    //CONVERTS THE RESULTS FROM STRING FORM TO OBJECT OR DATA
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    //IF THERES AN ERROR, RUN THIS CODE
+    console.log("SOMETHING IS BUGGIN` DA CODE:", error);
+  }
+}
 
 //function that randomnly picks one character with image when name is typed and button is clicked
-async function randomCharacter(){
+async function randomCharacter() {
+  try {
+    const response = await fetch(
+      "https://dragonball-api.com/api/characters?limit10"
+    );
 
-    try{ 
-
-    const response = await fetcch("https://dragonball-api.com/api");
-
-     if(!response.ok){
-            throw new Error("NO CHARACTER CALLED!");
-        }
-
-        const data = await response.json();
-        console.log();
+    if (!response.ok) {
+      throw new Error("NO CHARACTER CALLED!");
     }
 
-    catch(error){
-        //IF THERES AN ERROR, RUN THIS CODE
-        console.log("SOMETHING IS WRONG WITH DA CODE:",error);
-    }
-
-    
+    const data = await response.json();
+    console.log(data.items);
+  } catch (error) {
+    //IF THERES AN ERROR, RUN THIS CODE
+    console.log("SOMETHING IS WRONG WITH YA CODE:", error);
+  }
 }
-       //ACTIVATE FUNCTIONS
-       document.getElementById("bttn").addEventListener("click",fecthData);
+//ACTIVATE FUNCTIONS
+
+
+async function showCharacters() {
+  const data = await fetchData(dragonName.value);
+
+  //CALL FUNCTION TO RENDER DATA
+  renderRandomDragon(data);
+}
+
+function renderRandomDragon(characterData) {
+  console.log(characterData);
+
+  //WRITE LOGIC TO RENDER DATA FROM CHARACTER
+}
